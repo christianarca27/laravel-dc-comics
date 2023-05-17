a06e6a00b1345ff9fe8db124b773936c55e6737f
christianarca27 <christian.arca.mail@gmail.com>
2023-05-17 14:23:37 +0200

    feat: stilizzati form di creazione e modifica tramite Bootstrap

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,58 +3,60 @@
 @section('content')
     <main>
         <div class="container">
-            <h1>Inserisci nuovo fumetto</h1>
+            <h1 class="mb-5">Inserisci nuovo fumetto</h1>
 
-            <form action="{{ route('comics.store') }}" method="post">
+            <form action="{{ route('comics.store') }}" method="post" class="mb-5">
                 @csrf
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="title">Titolo</label>
-                    <input type="text" name="title" id="title">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="title">Titolo</label>
+                    <input class="form-control" type="text" name="title" id="title">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="description">Descrizione</label>
-                    <input type="text" name="description" id="description">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="description">Descrizione</label>
+                    <input class="form-control" type="text" name="description" id="description">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="thumb">Thumb</label>
-                    <input type="text" name="thumb" id="thumb">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="thumb">Thumb</label>
+                    <input class="form-control" type="text" name="thumb" id="thumb">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="price">Prezzo</label>
-                    <input type="text" name="price" id="price">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="price">Prezzo</label>
+                    <input class="form-control" type="text" name="price" id="price">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="series">Serie</label>
-                    <input type="text" name="series" id="series">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="series">Serie</label>
+                    <input class="form-control" type="text" name="series" id="series">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="sale_date">Data di vendita</label>
-                    <input type="text" name="sale_date" id="sale_date">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="sale_date">Data di vendita</label>
+                    <input class="form-control" type="text" name="sale_date" id="sale_date">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="type">Tipo</label>
-                    <input type="text" name="type" id="type">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="type">Tipo</label>
+                    <input class="form-control" type="text" name="type" id="type">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="artists">Artisti</label>
-                    <textarea name="artists" id="artists"></textarea>
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="artists">Artisti</label>
+                    <textarea class="form-control" name="artists" id="artists"></textarea>
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="writers">Scrittori</label>
-                    <textarea name="writers" id="writers"></textarea>
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="writers">Scrittori</label>
+                    <textarea class="form-control" name="writers" id="writers"></textarea>
                 </div>
 
                 <button class="btn btn-primary">Inserisci</button>
             </form>
+
+            <a href="{{ route('comics.index') }}">Torna all'elenco completo</a>
         </div>
     </main>
 @endsection
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -3,63 +3,67 @@
 @section('content')
     <main>
         <div class="container">
-            <h1>Inserisci nuovo fumetto</h1>
+            <h1 class="mb-5">Modifica fumetto</h1>
 
-            <form action="{{ route('comics.update', $comic) }}" method="post">
+            <form action="{{ route('comics.update', $comic) }}" method="post" class="mb-5">
                 @csrf
                 @method('PUT')
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="title">Titolo</label>
-                    <input type="text" name="title" id="title" value="{{ $comic->title }}">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="title">Titolo</label>
+                    <input class="form-control" type="text" name="title" id="title" value="{{ $comic->title }}">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="description">Descrizione</label>
-                    <input type="text" name="description" id="description" value="{{ $comic->description }}">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="description">Descrizione</label>
+                    <input class="form-control" type="text" name="description" id="description"
+                        value="{{ $comic->description }}">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="thumb">Thumb</label>
-                    <input type="text" name="thumb" id="thumb" value="{{ $comic->thumb }}">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="thumb">Thumb</label>
+                    <input class="form-control" type="text" name="thumb" id="thumb" value="{{ $comic->thumb }}">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="price">Prezzo</label>
-                    <input type="text" name="price" id="price" value="{{ $comic->price }}">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="price">Prezzo</label>
+                    <input class="form-control" type="text" name="price" id="price" value="{{ $comic->price }}">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="series">Serie</label>
-                    <input type="text" name="series" id="series" value="{{ $comic->series }}">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="series">Serie</label>
+                    <input class="form-control" type="text" name="series" id="series" value="{{ $comic->series }}">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="sale_date">Data di vendita</label>
-                    <input type="text" name="sale_date" id="sale_date" value="{{ $comic->sale_date }}">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="sale_date">Data di vendita</label>
+                    <input class="form-control" type="text" name="sale_date" id="sale_date"
+                        value="{{ $comic->sale_date }}">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="type">Tipo</label>
-                    <input type="text" name="type" id="type" value="{{ $comic->type }}">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="type">Tipo</label>
+                    <input class="form-control" type="text" name="type" id="type" value="{{ $comic->type }}">
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="artists">Artisti</label>
-                    <textarea name="artists" id="artists">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="artists">Artisti</label>
+                    <textarea class="form-control" name="artists" id="artists">
                         {{ $comic->artists }}
                     </textarea>
                 </div>
 
-                <div class="input-group mb-3 gap-3">
-                    <label class="w-25" for="writers">Scrittori</label>
-                    <textarea name="writers" id="writers">
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="writers">Scrittori</label>
+                    <textarea class="form-control" name="writers" id="writers">
                         {{ $comic->writers }}
                     </textarea>
                 </div>
 
                 <button class="btn btn-primary">Modifica</button>
             </form>
+
+            <a href="{{ route('comics.index') }}">Torna all'elenco completo</a>
         </div>
     </main>
 @endsection
